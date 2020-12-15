b1f77b0378ffbfe8f6ef498744246e23b544fb0b
Kevin d'Orange <kevin.dorange@gmail.com>
2020-12-15 12:55:12 +0100

    Improved tests architecture with a send_waitback command

--- tests/connect_actions.rs
+++ tests/connect_actions.rs
@@ -9,8 +9,6 @@ use std::time::{Duration, Instant};
 mod utils;
 use utils::TestServer;
 
-const TIMEOUT_DELAY: u16 = 3;
-
 /// Requirements:
 /// > If the Server does not receive a CONNECT packet within a reasonable amount
 /// > of time after the Network Connection is established
@@ -18,14 +16,14 @@ const TIMEOUT_DELAY: u16 = 3;
 #[async_std::test]
 async fn connect_timeout() {
     let server = TestServer::prepare(BrokerSettings {
-        keep_alive: TIMEOUT_DELAY,
+        keep_alive: utils::TIMEOUT_DELAY,
         ..Default::default()
     })
     .await;
     let mut stream = server.create_client().await.unwrap();
 
     let now = Instant::now();
-    let delay_with_tolerance = (TIMEOUT_DELAY as f32 * 1.5) as u64;
+    let delay_with_tolerance = (utils::TIMEOUT_DELAY as f32 * 1.5) as u64;
 
     if let Ok(packet) = Packet::decode(&mut stream).await {
         match packet {
@@ -49,7 +47,7 @@ async fn connect_timeout() {
 #[async_std::test]
 async fn mqtt_3_1_4_1() {
     let server = TestServer::prepare(BrokerSettings {
-        keep_alive: TIMEOUT_DELAY,
+        keep_alive: utils::TIMEOUT_DELAY,
         ..Default::default()
     })
     .await;
@@ -57,31 +55,12 @@ async fn mqtt_3_1_4_1() {
 
     // Send an invalid connect packet and wait for an immediate disconnection
     // from the server.
-    let packet = Packet::Connect(Default::default());
-    let mut buffer = Vec::new();
-    packet.encode(&mut buffer).await.unwrap();
-    buffer[0] |= 0b1111; // Invalidate the packet
-
-    while stream.write(&buffer).await.is_err() {}
-
-    // Wait for a response from the server within the next seconds
-    let delay_with_tolerance = Duration::from_secs((TIMEOUT_DELAY as f32 * 1.5) as u64);
-    let mut buf = vec![0u8; 1024];
-    let result = io::timeout(delay_with_tolerance, stream.read(&mut buf)).await;
-
-    match result {
-        Err(e) => match e.kind() {
-            ErrorKind::TimedOut => panic!("Server did not respond to invalid connect packet"),
-            _ => panic!("IO Error: {:?}", e),
-        },
-        Ok(0) => { /* Normal Disconnection */ }
-        Ok(_) => {
-            let mut buf = Cursor::new(buf);
-            let packet = Packet::decode(&mut buf).await.unwrap();
-            assert!(matches!(packet, Packet::ConnAck(_)));
-            if let Packet::ConnAck(packet) = packet {
-                assert_eq!(packet.reason_code, ReasonCode::MalformedPacket);
-            }
+    if let Some(packet) =
+        utils::send_waitback(&mut stream, Packet::Connect(Default::default()), true).await
+    {
+        assert!(matches!(packet, Packet::ConnAck(_)));
+        if let Packet::ConnAck(packet) = packet {
+            assert_eq!(packet.reason_code, ReasonCode::MalformedPacket);
         }
     }
 
@@ -100,7 +79,7 @@ async fn mqtt_3_1_4_2() {
     // Create a set of pairs with a server and an unsupported connect packet.
     // Each one will be tested against an expected ConnAck > 0x80
     let settings = BrokerSettings {
-        keep_alive: TIMEOUT_DELAY,
+        keep_alive: utils::TIMEOUT_DELAY,
         ..Default::default()
     };
     // Vector of (input,output) tests.
@@ -131,35 +110,15 @@ async fn mqtt_3_1_4_2() {
         let server = TestServer::prepare(settings).await;
         let mut stream = server.create_client().await.unwrap();
 
-        // Send an invalid connect packet and wait for an immediate disconnection
+        // Send an unsupported connect packet and wait for an immediate disconnection
         // from the server.
-        let packet = Packet::from(connect);
-        let mut buffer = Vec::new();
-        packet.encode(&mut buffer).await.unwrap();
-        while stream.write(&buffer).await.is_err() {}
-
-        // Wait for a response from the server within the next seconds
-        let delay_with_tolerance = Duration::from_secs((TIMEOUT_DELAY as f32 * 1.5) as u64);
-        let mut buf = vec![0u8; 1024];
-        let result = io::timeout(delay_with_tolerance, stream.read(&mut buf)).await;
-
-        match result {
-            Err(e) => match e.kind() {
-                ErrorKind::TimedOut => panic!("Server did not respond to invalid connect packet"),
-                _ => panic!("IO Error: {:?}", e),
-            },
-            Ok(0) => { /* Normal Disconnection */ }
-            Ok(_) => {
-                let mut buf = Cursor::new(buf);
-                let packet = Packet::decode(&mut buf).await.unwrap();
-                if let Packet::ConnAck(packet) = packet {
-                    assert_eq!(packet.reason_code, reason_code);
-                } else {
-                    panic!("Packet should be ConnAck");
-                }
+        if let Some(packet) = utils::send_waitback(&mut stream, connect.into(), false).await {
+            if let Packet::ConnAck(packet) = packet {
+                assert_eq!(packet.reason_code, reason_code);
+            } else {
+                panic!("Packet should be ConnAck");
             }
         }
-
         server.stop().await;
     }
 }
@@ -171,7 +130,7 @@ async fn mqtt_3_1_4_2() {
 #[async_std::test]
 async fn mqtt_3_1_4_3() {
     let server = TestServer::prepare(BrokerSettings {
-        keep_alive: TIMEOUT_DELAY,
+        keep_alive: utils::TIMEOUT_DELAY,
         ..Default::default()
     })
     .await;
@@ -179,34 +138,19 @@ async fn mqtt_3_1_4_3() {
 
     ////////////////////////////////////////////////////////////////////////////
     // Using the first client, we send a Connect packet and wait for the ConnAck
-    let packet = Packet::Connect(Connect {
+    let connect = Connect {
         client_id: Some("Suzuki".into()),
         ..Default::default()
-    });
-    let mut buffer = Vec::new();
-    packet.encode(&mut buffer).await.unwrap();
-    while stream.write(&buffer).await.is_err() {}
+    };
 
-    // Wait for a response from the server within the next seconds
-    let delay_with_tolerance = Duration::from_secs((TIMEOUT_DELAY as f32 * 1.5) as u64);
-    let mut buf = vec![0u8; 1024];
-    let result = io::timeout(delay_with_tolerance, stream.read(&mut buf)).await;
-
-    match result {
-        Err(e) => match e.kind() {
-            ErrorKind::TimedOut => panic!("Server did not respond to connect packet"),
-            _ => panic!("IO Error: {:?}", e),
-        },
-        Ok(0) => panic!("Server shut down connexion after Connect packet is sent"),
-        Ok(_) => {
-            let mut buf = Cursor::new(buf);
-            let packet = Packet::decode(&mut buf).await.unwrap();
-            if let Packet::ConnAck(packet) = packet {
-                assert_eq!(packet.reason_code, ReasonCode::Success);
-            } else {
-                panic!("Invalid packet type sent after Connect");
-            }
-        }
+    // By unwrapping we ensure to panic! is the server disconnected
+    if let Packet::ConnAck(packet) = utils::send_waitback(&mut stream, connect.into(), false)
+        .await
+        .unwrap()
+    {
+        assert_eq!(packet.reason_code, ReasonCode::Success);
+    } else {
+        panic!("Invalid packet type sent after Connect");
     }
 
     ////////////////////////////////////////////////////////////////////////////
--- tests/utils.rs
+++ tests/utils.rs
@@ -1,9 +1,11 @@
 use async_std::{
+    io::{self, prelude::*, Cursor, ErrorKind},
     net::{SocketAddr, TcpStream},
     sync::Arc,
     task::{self, JoinHandle},
 };
 use sage_broker::{service, Broker, BrokerSettings};
+use sage_mqtt::Packet;
 use std::time::Duration;
 
 pub struct TestServer {
@@ -11,6 +13,7 @@ pub struct TestServer {
     service_task: JoinHandle<()>,
     pub local_addr: SocketAddr,
 }
+pub const TIMEOUT_DELAY: u16 = 3;
 
 impl TestServer {
     pub async fn prepare(settings: BrokerSettings) -> TestServer {
@@ -45,5 +48,38 @@ impl TestServer {
     pub async fn stop(self) {
         self.broker.shutdown().await;
         self.service_task.await;
+    }
+}
+
+/// Sends the given packet and wait for the next response from the server.
+/// Note that nothing ensures the received packet from the server is a response
+/// to the sent packet.
+pub async fn send_waitback(
+    stream: &mut TcpStream,
+    packet: Packet,
+    invalid: bool,
+) -> Option<Packet> {
+    // Send the packet as buffer
+    let mut buffer = Vec::new();
+    packet.encode(&mut buffer).await.unwrap();
+
+    if invalid {
+        buffer[0] |= 0b1111; // Invalidate the packet
+                             // TODO Assert the packet IS invalid
+    }
+    while stream.write(&buffer).await.is_err() {} // TODO Should we timeout this?
+
+    let delay_with_tolerance = Duration::from_secs((TIMEOUT_DELAY as f32 * 1.5) as u64);
+    let mut buf = vec![0u8; 1024];
+    match io::timeout(delay_with_tolerance, stream.read(&mut buf)).await {
+        Err(e) => match e.kind() {
+            ErrorKind::TimedOut => panic!("Server did not respond to invalid connect packet"),
+            _ => panic!("IO Error: {:?}", e),
+        },
+        Ok(0) => None,
+        Ok(_) => {
+            let mut buf = Cursor::new(buf);
+            Some(Packet::decode(&mut buf).await.unwrap())
+        }
     }
 }
