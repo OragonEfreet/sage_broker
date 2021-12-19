0bb6265cf9e40f36ba960a834d06bd14f2613a70
Kevin d'Orange <kdorange@techsoft3d.com>
2021-12-19 11:26:04 +0100

    Ack Connect code is in control::connect

--- src/broker_settings.rs
+++ src/broker_settings.rs
@@ -1,6 +1,4 @@
-use nanoid::nanoid;
-use sage_mqtt::{defaults, ConnAck, Connect, QoS, ReasonCode};
-use std::cmp::min;
+use sage_mqtt::{defaults, QoS};
 
 /// Configuration structure for a broker.
 /// This structure is used to customize the behaviour of your broker. It is used
@@ -81,104 +79,6 @@ impl Default for BrokerSettings {
             maximum_packet_size: None,
             topic_alias_maximum: defaults::DEFAULT_TOPIC_ALIAS_MAXIMUM,
             force_keep_alive: false,
-        }
-    }
-}
-
-impl BrokerSettings {
-    /// Generates an acknowledgement packet given the `connect` packet and the
-    /// current configuration.
-    pub fn acknowledge_connect(&self, connect: &Connect) -> ConnAck {
-        // If the server forces the value, we use it.
-        // Otherwise we take the value from the connect request or
-        // the server one if absent.
-        let session_expiry_interval = {
-            if self.force_session_expiry_interval {
-                self.session_expiry_interval
-            } else {
-                connect
-                    .session_expiry_interval
-                    .or(self.session_expiry_interval)
-            }
-        };
-
-        // The maximum receive_maximum if the minimum value between server and
-        // client requirements
-        let receive_maximum = min(connect.receive_maximum, self.receive_maximum);
-
-        // The maximum packet size is either the minimal value of client and
-        // server requirements or the only value defined by one of them, or None
-        let maximum_packet_size = match (connect.maximum_packet_size, self.maximum_packet_size) {
-            (Some(a), Some(b)) => Some(min(a, b)),
-            (None, Some(a)) | (Some(a), None) => Some(a),
-            _ => None,
-        };
-
-        // If the client did not specify a client ID, the server must generate
-        // and assign one.
-        let assigned_client_id = if connect.client_id.is_some() {
-            None
-        } else {
-            Some(format!("sage_mqtt-{}", nanoid!()))
-        };
-
-        // The topic alias maximum if the minimum value between server and client
-        // requirements.
-        let topic_alias_maximum = min(self.topic_alias_maximum, connect.topic_alias_maximum);
-
-        // Values directly defined by the configuration
-        let maximum_qos = self.maximum_qos;
-        let retain_available = self.retain_enabled;
-
-        // The keep alive value is given by the connect packet but
-        // can be force overriden by the server
-        let keep_alive = if self.force_keep_alive {
-            Some(self.keep_alive)
-        } else {
-            None
-        };
-
-        // Enhanced authentication is not supported for now
-        let (reason_code, reason_string) = {
-            if connect.authentication.is_some() || connect.user_name.is_some() {
-                (
-                    ReasonCode::BadAuthenticationMethod,
-                    Some("Enhanced anthentication non supported".into()),
-                )
-            } else {
-                (ReasonCode::Success, None)
-            }
-        };
-
-        let wildcard_subscription_available = false;
-        let subscription_identifiers_available = false;
-        let shared_subscription_available = false;
-        let response_information = None;
-        let reference = None;
-
-        let session_present = false;
-        let user_properties = Default::default();
-        let authentication = None;
-
-        ConnAck {
-            reason_code,
-            session_expiry_interval,
-            receive_maximum,
-            maximum_qos,
-            retain_available,
-            maximum_packet_size,
-            assigned_client_id,
-            topic_alias_maximum,
-            reason_string,
-            wildcard_subscription_available,
-            subscription_identifiers_available,
-            shared_subscription_available,
-            keep_alive,
-            response_information,
-            reference,
-            session_present,
-            user_properties,
-            authentication,
         }
     }
 }
--- src/control/connect.rs
+++ src/control/connect.rs
@@ -1,6 +1,8 @@
 use crate::{BrokerSettings, Peer, Session, Sessions};
 use async_std::sync::{Arc, RwLock};
-use sage_mqtt::{Connect, Disconnect, ReasonCode};
+use nanoid::nanoid;
+use sage_mqtt::{ConnAck, Connect, Disconnect, ReasonCode};
+use std::cmp::min;
 
 pub async fn run(
     connect: Connect,
@@ -10,7 +12,7 @@ pub async fn run(
 ) {
     // First, we prepare an first connack using broker policy
     // and infer the actual client_id requested for this client
-    let mut connack = settings.acknowledge_connect(&connect);
+    let mut connack = acknowledge_connect(settings, &connect);
 
     if connack.reason_code == ReasonCode::Success {
         let client_id = connack
@@ -59,5 +61,99 @@ pub async fn run(
         peer.send(connack.into()).await;
     } else {
         peer.send_close(connack.into()).await;
+    }
+}
+
+fn acknowledge_connect(settings: Arc<BrokerSettings>, connect: &Connect) -> ConnAck {
+    // If the server forces the value, we use it.
+    // Otherwise we take the value from the connect request or
+    // the server one if absent.
+    let session_expiry_interval = {
+        if settings.force_session_expiry_interval {
+            settings.session_expiry_interval
+        } else {
+            connect
+                .session_expiry_interval
+                .or(settings.session_expiry_interval)
+        }
+    };
+
+    // The maximum receive_maximum if the minimum value between server and
+    // client requirements
+    let receive_maximum = min(connect.receive_maximum, settings.receive_maximum);
+
+    // The maximum packet size is either the minimal value of client and
+    // server requirements or the only value defined by one of them, or None
+    let maximum_packet_size = match (connect.maximum_packet_size, settings.maximum_packet_size) {
+        (Some(a), Some(b)) => Some(min(a, b)),
+        (None, Some(a)) | (Some(a), None) => Some(a),
+        _ => None,
+    };
+
+    // If the client did not specify a client ID, the server must generate
+    // and assign one.
+    let assigned_client_id = if connect.client_id.is_some() {
+        None
+    } else {
+        Some(format!("sage_mqtt-{}", nanoid!()))
+    };
+
+    // The topic alias maximum if the minimum value between server and client
+    // requirements.
+    let topic_alias_maximum = min(settings.topic_alias_maximum, connect.topic_alias_maximum);
+
+    // Values directly defined by the configuration
+    let maximum_qos = settings.maximum_qos;
+    let retain_available = settings.retain_enabled;
+
+    // The keep alive value is given by the connect packet but
+    // can be force overriden by the server
+    let keep_alive = if settings.force_keep_alive {
+        Some(settings.keep_alive)
+    } else {
+        None
+    };
+
+    // Enhanced authentication is not supported for now
+    let (reason_code, reason_string) = {
+        if connect.authentication.is_some() || connect.user_name.is_some() {
+            (
+                ReasonCode::BadAuthenticationMethod,
+                Some("Enhanced anthentication non supported".into()),
+            )
+        } else {
+            (ReasonCode::Success, None)
+        }
+    };
+
+    let wildcard_subscription_available = false;
+    let subscription_identifiers_available = false;
+    let shared_subscription_available = false;
+    let response_information = None;
+    let reference = None;
+
+    let session_present = false;
+    let user_properties = Default::default();
+    let authentication = None;
+
+    ConnAck {
+        reason_code,
+        session_expiry_interval,
+        receive_maximum,
+        maximum_qos,
+        retain_available,
+        maximum_packet_size,
+        assigned_client_id,
+        topic_alias_maximum,
+        reason_string,
+        wildcard_subscription_available,
+        subscription_identifiers_available,
+        shared_subscription_available,
+        keep_alive,
+        response_information,
+        reference,
+        session_present,
+        user_properties,
+        authentication,
     }
 }
